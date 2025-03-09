6d097416d0b22f06287c8193d8c2c8e49851a9d9
simon arch desktop <simon.janousekk@gmail.com>
2025-03-09 15:42:04 +0100

    nevim

--- init.lua
+++ init.lua
@@ -104,6 +104,10 @@ vim.g.have_nerd_font = false
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
